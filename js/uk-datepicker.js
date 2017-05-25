ae298224813fc85f54614813b57941defd75a70c
Tomasz Bakula <tbakula92@gmail.com>
2017-05-25 11:05:48 +0100

    Add support for repeatable fields.

--- js/uk-datepicker.js
+++ js/uk-datepicker.js
@@ -1,7 +1,17 @@
 CMB.addCallbackForInit( function() {
 
-	// Datepicker
-	jQuery('.cgit_uk_datepicker').datepicker({ dateFormat: 'dd/mm/yy' });
+	jQuery('body').on('focus', '.cgit_uk_datepicker', function () {
+		jQuery(this).datepicker({ dateFormat: 'dd/mm/yy' });
+	});
+
+	jQuery('body').on('focus', '.cmb_timepicker', function () {
+		jQuery(this).timePicker({
+			startTime: "00:00",
+			endTime: "23:30",
+			show24Hours: false,
+			separator: ':',
+			step: 30
+		});
+	});
 
 });
-
